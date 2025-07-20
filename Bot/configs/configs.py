a114fc39d8c95d2ba68779a8fa837d760e4a551d
MuriloBahr <qwsf.murilo@gmail.com>
2025-07-20 20:45:07 -0300

    added news xpaths in file configs

--- Bot/configs/configs.py
+++ Bot/configs/configs.py
@@ -20,8 +20,17 @@ class Config:
     XPATH_INPUT_NAME = '//*[@id="screenHome"]/div[2]/div[1]/div[2]/input'
     XPATH_THEME_LIST = '//*[@id="screenCreate"]/div[2]/div[2]/div[2]/div[1]/div'
     XPATH_INSERT_THEMES = '//*[@id="screenCreate"]/div[2]/div[2]/form/input[1]'
+    XPATH_PASSWORD = '//*[@id="screenCreate"]/div[2]/div[1]/div[4]/span/input'
+    XPATH_CLICK_CREATE_ROOM = '//*[@id="screenCreate"]/div[3]/button'
+    XPATH_LINK_ROOM = '//*[@id="popup"]/div/div/div[2]/div/div[1]/input'
+    XPATH_CLOSE_LINK_ROOM = '//*[@id="popup"]/div/div/button'
+    XPATH_CHAT = '//*[@id="chat"]/form/input'
+    XPATH_START_GAME = '//*[@id="screenGame"]/div[2]/div[2]/div/button'
+    XPATH_LIST_USERS = '//*[@id="users"]'
     
     THEMES: list = field(default_factory=list)
+    
+    DATA_JSON: dict = field(default_factory=dict)
 
     def __post_init__(self):
         self.load_json()
@@ -30,6 +39,7 @@ class Config:
         with open(os.path.join(self.BASE_DIR, "json_config.json"), "r") as f:
             data = json.load(f)
             self.THEMES = data["config_stop"]["THEMES"]
+            self.DATA_JSON = data
 
 config = Config()
     
