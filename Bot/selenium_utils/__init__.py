bef99a2ef74c387f5e72168f2a29aa3ff43f441f
MuriloBahr <qwsf.murilo@gmail.com>
2025-07-20 20:46:06 -0300

    added new functions in selenium utils file

--- Bot/selenium_utils/__init__.py
+++ Bot/selenium_utils/__init__.py
@@ -1,23 +1,34 @@
 from dataclasses import dataclass
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from configs.configs import config
 
 
 @dataclass
 class UtilsSelenium():
     
+    driver: any
+    wait_time: int = 30
+    last_list_html = "" 
     
+
     def click_button(self, xpath):
         try:
-            button = self.driver.find_element(By.XPATH, xpath)
+            button = WebDriverWait(self.driver, self.wait_time).until(
+                EC.element_to_be_clickable((By.XPATH, xpath))
+            )
             button.click()
             print('@@ CLICK SUCCESSFULLY @@')
         except Exception as e:
             print(f'Error: {e}')
-    
+
     def insert_input(self, content, xpath, enter=False):
         try:
-            insert = self.driver.find_element(By.XPATH, xpath)
+            insert = WebDriverWait(self.driver, self.wait_time).until(
+                EC.visibility_of_element_located((By.XPATH, xpath))
+            )
             insert.clear()
             insert.send_keys(content)
             if enter:
@@ -25,4 +36,24 @@ class UtilsSelenium():
             print('@@ INSERT SUCCESSFULLY @@')
         except Exception as e:
             print(f'ERROR: {e}')
-        
+
+    def get_input_value(self, xpath):
+        value = None
+        try:
+            input_element = WebDriverWait(self.driver, self.wait_time).until(
+                EC.presence_of_element_located((By.XPATH, xpath))
+            )
+            value = input_element.get_attribute("value")
+        except Exception as e:
+            print(f'ERROR: {e}')
+        return value
+    
+    def content_changed(self, xpath):
+        try:
+            current_content = self.driver.find_element(By.XPATH, xpath).get_attribute("innerHTML")
+            if self.last_list_html != current_content:
+                self.last_list_html = current_content
+                return True
+            return False
+        except Exception:
+            return False
